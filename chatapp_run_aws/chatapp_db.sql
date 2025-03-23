d88a3881c09964a403b993b40f51b7f7d1fd7b91
Hauharu <trunghauu71@gmail.com>
2025-03-24 01:15:42 +0700

    up

--- chatapp_run_aws/chatapp_db.sql
+++ chatapp_run_aws/chatapp_db.sql
@@ -2,7 +2,7 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 0.0.0.0
+-- Host: 127.0.0.1
 -- Generation Time: Mar 17, 2025 at 06:55 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
@@ -10,7 +10,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -35,6 +34,7 @@ CREATE TABLE `messages` (
   `msg_img` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user_form`
@@ -50,6 +50,9 @@ CREATE TABLE `user_form` (
   `status` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Indexes for dumped tables
+--
 
 --
 -- Indexes for table `messages`
@@ -79,13 +82,14 @@ ALTER TABLE `messages`
 ALTER TABLE `user_form`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
--- Cho phép NULL cho msg_img
+-- ✅ Cho phép NULL cho msg_img
 ALTER TABLE messages MODIFY msg_img VARCHAR(255) NULL;
 
--- Cho phép NULL cho msg
+-- ✅ Cho phép NULL cho msg
 ALTER TABLE messages MODIFY msg TEXT NULL;
+
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
